2773b7be68b9c8c6c4a08d11796e60010a96d453
Thomas Lucking <telucking@gmail.comm>
2025-04-29 07:25:18 +0200

    Updated

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// To download this dependency.
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeScreen from './Home';
+import Login from './Login';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Login: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
