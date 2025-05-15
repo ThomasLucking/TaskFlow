752332a57232deb679bea1668bec7494b770ea2f
Thomas Lucking <telucking@gmail.com>
2025-05-15 13:08:59 +0200

    Disabled swipe once logged in

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,9 @@ const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}  />
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}  />
-        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }}  />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false, gestureEnabled: false }}  />
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false, gestureEnabled: false }}  />
+        <Stack.Screen name="Register" component={Register} options={{ headerShown: false, gestureEnabled: false }}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
