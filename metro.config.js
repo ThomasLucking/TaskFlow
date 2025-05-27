fb605a74bec405305c45f99680f66287ca5d9903
Thomas Lucking <telucking@gmail.com>
2025-05-27 12:51:41 +0200

    Improved performance

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,9 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  transformer: {
+    maxWorkerCount: 6,
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
